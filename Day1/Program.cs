e7a64cca620fa99c469c7f42510757e142918bdf
Peter <peter.d.champion.jr@gmail.com>
2025-06-02 17:02:04 -0500

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a .NET 9.0 application using C# 13.0 language features.");
+
+
+//Writes a comment to the console for the user to imput a comment and reads it back 
+Console.WriteLine("Please Input a comment:");
+string comment = "";
+comment = Console.ReadLine();
+Console.WriteLine("You entered: " + comment);
+
+// This simple program demonstrates the usage of C# 13 and .NET 9.0 To print a simple message to the console and read user input.
